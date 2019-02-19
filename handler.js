5a02736c0a62ef538d6c4783db0a05a2ac9593f0
Blake Danson <blakedanson@gmail.com>
2019-02-19 07:53:45 -0800

    Return mock data object

--- handler.js
+++ handler.js
@@ -1,14 +1,40 @@
 'use strict';
 
-module.exports.hello = async (event, context) => {
+function calculateValues(){
+  let data = {
+    summary: {
+      grm: 10,
+      costPerUnit: 100000,
+      capRate: 0.06,
+      cashOnCash: 0.04,
+      dscr: 1.2,
+      appreciation: 0.02,
+      depreciation: 0.02,
+      amortization: 0.02,
+      unleveragedIRR: 10,
+      leveragedIRR: 10
+    },
+    healthCheck: {
+      totalExpenses: 0.4,
+      annualRepairs: 100000,
+      vacancyLoss: 0.04,
+      insurance: 1.2,
+      kBRehabs: 3000
+    }
+  }
+  return data;
+}
+
+module.exports.calc = async (event, context) => {
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: calculateValues(),
       input: event,
     }),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
